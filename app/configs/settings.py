1f90cba4ae4bfee080e3eb8526000684ba561f43
Takuro Huang <takuro1026@gmail.com>
2023-11-17 06:51:22 +0800

    Update env-example and settings

--- app/configs/settings.py
+++ app/configs/settings.py
@@ -7,13 +7,8 @@ def get_settings():
     return Settings()
 
 class Settings(BaseSettings):
-    BASE_MODEL_PATH: str
-    LORA_MODEL_PATH: str
-    FOLDER_TOKENIZER_PATH: str
-    DATASET_BASE_PATH: str
-    TAGGERS_BASE_PATH: str
-    BITS: int
-    GPU_INDEX: int = 0
-    FN_OUT_TSV: str = ""
+    SOMETHING_KEY: str
+    SOMETHING_PATH: str
+
 
     model_config = SettingsConfigDict(env_file="./app/config/settings.env")
